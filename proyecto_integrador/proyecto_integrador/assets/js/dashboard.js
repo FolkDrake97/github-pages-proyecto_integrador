7bc481815095e19f4085ac9c69664b15b6a2b08a
FolkDrake 97 <143561012+FolkDrake97@users.noreply.github.com>
2025-11-18 10:42:56 -0600

    Delete proyecto_integrador/proyecto_integrador directory

--- proyecto_integrador/proyecto_integrador/assets/js/dashboard.js
+++
@@ -1,6 +0,0 @@
-// Dashboard JavaScript
-// Este archivo puede contener funciones específicas del dashboard
-
-console.log('Dashboard JS cargado correctamente');
-
-// Aquí puedes agregar funciones específicas si las necesitas más adelante
